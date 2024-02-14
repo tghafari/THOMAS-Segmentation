75d4ac75eecfbd2647816651da49182dc91c13c2
tghafari <tara.ghafari@gmail.com>
2024-02-14 10:47:20 +0000

    modified for more general use

--- run_thomas_segmentation.sh
+++ run_thomas_segmentation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --account bagshaap-eeg-fmri-hmm
+#SBATCH --account jenseno-avtemporal-attention
 #SBATCH --qos bbdefault
 #SBATCH --time 120
 #SBATCH --nodes 1  # ensure the job runs on a single node
@@ -8,6 +8,24 @@
 #SBATCH --mem 48G
 #SBATCH --constraint icelake
 
+# This code segments thalamus running hipsthomas in singularity container
+# input_dir is where you read the T1w file
+# output_dir is where you'd save the output
+# singularity_dir is where you saved the hipsthomas container
+
+# Define input and output directories
+input_dir="/rds/projects/q/quinna-camcan/cc700/mri/pipeline/release004/BIDS_20190411/anat/sub-CC110037/anat"
+output_dir="/rds/projects/j/jenseno-avtemporal-attention/Projects/subcortical-structures/attention-striatum-7T/results"
+singularity_dir="/rds/projects/j/jenseno-avtemporal-attention/Projects/subcortical-structures/attention-striatum-7T" 
+
 set -e
 
-singularity run -B ${PWD}:${PWD} -W ${PWD} -u --cleanenv hipsthomas.sif bash -c "hipsthomas_csh -i sub-20240201c3e8_T1w.nii.gz -t1 -big" 
+# Run the Singularity command
+singularity run \
+    -B "${input_dir}:${input_dir}" \
+    -B "${output_dir}:${output_dir}" \
+    -W "${input_dir}" \
+    -u --cleanenv "${singularity_dir}/hipsthomas.sif" bash -c \
+    "hipsthomas_csh \
+    -i ${input_dir}/sub-CC110037_T1w.nii.gz \
+    -t1 -big > ${output_dir}/THOMAS.log"
